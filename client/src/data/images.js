09bc7b56c4ed7613b948e190d08bce0bcf50e881
Kevin Penney <kpenney@gmail.com>
2021-03-08 21:39:46 -0800

    stuff

---
+++ client/src/data/images.js
@@ -0,0 +1,194 @@
+const img1 = '../IMG/portfolio-tiny/1.jpg'
+const img1Full = '../IMG/portfolio-large-tiny/1.jpg'
+const img2 = '../IMG/portfolio-tiny/2.jpg'
+const img2Full = '../IMG/portfolio-large-tiny/2.jpg'
+const img3 = '../IMG/portfolio-tiny/3.jpg'
+const img3Full = '../IMG/portfolio-large-tiny/3.jpg'
+const img4 = '../IMG/portfolio-tiny/4.jpg'
+const img4Full = '../IMG/portfolio-large-tiny/4.jpg'
+const img5 = '../IMG/portfolio-tiny/5.jpg'
+const img5Full = '../IMG/portfolio-large-tiny/5.jpg'
+const img6 = '../IMG/portfolio-tiny/6.jpg'
+const img6Full = '../IMG/portfolio-large-tiny/6.jpg'
+const img7 = '../IMG/portfolio-tiny/7.jpg'
+const img7Full = '../IMG/portfolio-large-tiny/7.jpg'
+const img8 = '../IMG/portfolio-tiny/8.jpg'
+const img8Full = '../IMG/portfolio-large-tiny/8.jpg'
+const img9 = '../IMG/portfolio-tiny/9.jpg'
+const img9Full = '../IMG/portfolio-large-tiny/9.jpg'
+const img10 = '../IMG/portfolio-tiny/10.jpg'
+const img10Full = '../IMG/portfolio-large-tiny/10.jpg'
+const img11 = '../IMG/portfolio-tiny/11.jpg'
+const img11Full = '../IMG/portfolio-large-tiny/11.jpg'
+const img12 = '../IMG/portfolio-tiny/12.jpg'
+const img12Full = '../IMG/portfolio-large-tiny/12.jpg'
+const img13 = '../IMG/portfolio-tiny/13.jpg'
+const img13Full = '../IMG/portfolio-large-tiny/13.jpg'
+const img14 = '../IMG/portfolio-tiny/14.jpg'
+const img14Full = '../IMG/portfolio-large-tiny/14.jpg'
+const img15 = '../IMG/portfolio-tiny/15.jpg'
+const img15Full = '../IMG/portfolio-large-tiny/15.jpg'
+const img16 = '../IMG/portfolio-tiny/16.jpg'
+const img16Full = '../IMG/portfolio-large-tiny/16.jpg'
+const img17 = '../IMG/portfolio-tiny/17.jpg'
+const img17Full = '../IMG/portfolio-large-tiny/17.jpg'
+const img18 = '../IMG/portfolio-tiny/18.jpg'
+const img18Full = '../IMG/portfolio-large-tiny/18.jpg'
+const img19 = '../IMG/portfolio-tiny/19.jpg'
+const img19Full = '../IMG/portfolio-large-tiny/19.jpg'
+const img20 = '../IMG/portfolio-tiny/20.jpg'
+const img20Full = '../IMG/portfolio-large-tiny/20.jpg'
+const img21 = '../IMG/portfolio-tiny/21.jpg'
+const img21Full = '../IMG/portfolio-large-tiny/21.jpg'
+const img22 = '../IMG/portfolio-tiny/22.jpg'
+const img22Full = '../IMG/portfolio-large-tiny/22.jpg'
+const img23 = '../IMG/portfolio-tiny/23.jpg'
+const img23Full = '../IMG/portfolio-large-tiny/23.jpg'
+const img24 = '../IMG/portfolio-tiny/24.jpg'
+const img24Full = '../IMG/portfolio-large-tiny/24.jpg'
+const img25 = '../IMG/portfolio-tiny/25.jpg'
+const img25Full = '../IMG/portfolio-large-tiny/25.jpg'
+const img26 = '../IMG/portfolio-tiny/26.jpg'
+const img26Full = '../IMG/portfolio-large-tiny/26.jpg'
+const img27 = '../IMG/portfolio-tiny/27.jpg'
+const img27Full = '../IMG/portfolio-large-tiny/27.jpg'
+const img28 = '../IMG/portfolio-tiny/28.jpg'
+const img28Full = '../IMG/portfolio-large-tiny/28.jpg'
+const img29 = '../IMG/portfolio-tiny/29.jpg'
+const img29Full = '../IMG/portfolio-large-tiny/29.jpg'
+const img30 = '../IMG/portfolio-tiny/30.jpg'
+const img30Full = '../IMG/portfolio-large-tiny/30.jpg'
+const img31 = '../IMG/portfolio-tiny/31.jpg'
+const img31Full = '../IMG/portfolio-large-tiny/31.jpg'
+const img32 = '../IMG/portfolio-tiny/32.jpg'
+const img32Full = '../IMG/portfolio-large-tiny/32.jpg'
+const img33 = '../IMG/portfolio-tiny/33.jpg'
+const img33Full = '../IMG/portfolio-large-tiny/33.jpg'
+const img34 = '../IMG/portfolio-tiny/34.jpg'
+const img34Full = '../IMG/portfolio-large-tiny/34.jpg'
+const img35 = '../IMG/portfolio-tiny/35.jpg'
+const img35Full = '../IMG/portfolio-large-tiny/35.jpg'
+const img36 = '../IMG/portfolio-tiny/36.jpg'
+const img36Full = '../IMG/portfolio-large-tiny/36.jpg'
+const img37 = '../IMG/portfolio-tiny/37.jpg'
+const img37Full = '../IMG/portfolio-large-tiny/37.jpg'
+const img38 = '../IMG/portfolio-tiny/38.jpg'
+const img38Full = '../IMG/portfolio-large-tiny/38.jpg'
+const img39 = '../IMG/portfolio-tiny/39.jpg'
+const img39Full = '../IMG/portfolio-large-tiny/39.jpg'
+const img40 = '../IMG/portfolio-tiny/40.jpg'
+const img40Full = '../IMG/portfolio-large-tiny/40.jpg'
+const img41 = '../IMG/portfolio-tiny/41.jpg'
+const img41Full = '../IMG/portfolio-large-tiny/41.jpg'
+const img42 = '../IMG/portfolio-tiny/42.jpg'
+const img42Full = '../IMG/portfolio-large-tiny/42.jpg'
+const img43 = '../IMG/portfolio-tiny/43.jpg'
+const img43Full = '../IMG/portfolio-large-tiny/43.jpg'
+const img44 = '../IMG/portfolio-tiny/44.jpg'
+const img44Full = '../IMG/portfolio-large-tiny/44.jpg'
+const img45 = '../IMG/portfolio-tiny/45.jpg'
+const img45Full = '../IMG/portfolio-large-tiny/45.jpg'
+const img46 = '../IMG/portfolio-tiny/46.jpg'
+const img46Full = '../IMG/portfolio-large-tiny/46.jpg'
+const img47 = '../IMG/portfolio-tiny/47.jpg'
+const img47Full = '../IMG/portfolio-large-tiny/47.jpg'
+const img48 = '../IMG/portfolio-tiny/48.jpg'
+const img48Full = '../IMG/portfolio-large-tiny/48.jpg'
+const img49 = '../IMG/portfolio-tiny/49.jpg'
+const img49Full = '../IMG/portfolio-large-tiny/49.jpg'
+const img50 = '../IMG/portfolio-tiny/50.jpg'
+const img50Full = '../IMG/portfolio-large-tiny/50.jpg'
+const img51 = '../IMG/portfolio-tiny/51.jpg'
+const img51Full = '../IMG/portfolio-large-tiny/51.jpg'
+const img52 = '../IMG/portfolio-tiny/52.jpg'
+const img52Full = '../IMG/portfolio-large-tiny/52.jpg'
+const img53 = '../IMG/portfolio-tiny/53.jpg'
+const img53Full = '../IMG/portfolio-large-tiny/53.jpg'
+const img54 = '../IMG/portfolio-tiny/54.jpg'
+const img54Full = '../IMG/portfolio-large-tiny/54.jpg'
+const img55 = '../IMG/portfolio-tiny/55.jpg'
+const img55Full = '../IMG/portfolio-large-tiny/55.jpg'
+const img56 = '../IMG/portfolio-tiny/56.jpg'
+const img56Full = '../IMG/portfolio-large-tiny/56.jpg'
+const img57 = '../IMG/portfolio-tiny/57.jpg'
+const img57Full = '../IMG/portfolio-large-tiny/57.jpg'
+const img58 = '../IMG/portfolio-tiny/58.jpg'
+const img58Full = '../IMG/portfolio-large-tiny/58.jpg'
+const img59 = '../IMG/portfolio-tiny/59.jpg'
+const img59Full = '../IMG/portfolio-large-tiny/59.jpg'
+const img60 = '../IMG/portfolio-tiny/60.jpg'
+const img60Full = '../IMG/portfolio-large-tiny/60.jpg'
+const img61 = '../IMG/portfolio-tiny/61.jpg'
+const img61Full = '../IMG/portfolio-large-tiny/61.jpg'
+const img62 = '../IMG/portfolio-tiny/62.jpg'
+const img62Full = '../IMG/portfolio-large-tiny/62.jpg'
+const img63 = '../IMG/portfolio-tiny/63.jpg'
+const img63Full = '../IMG/portfolio-large-tiny/63.jpg'
+
+const images = [
+   { id: 1, src: img1, full: img1Full },
+   { id: 2, src: img2, full: img2Full },
+   { id: 3, src: img3, full: img3Full },
+   { id: 4, src: img4, full: img4Full },
+   { id: 5, src: img5, full: img5Full },
+   { id: 6, src: img6, full: img6Full },
+   { id: 7, src: img7, full: img7Full },
+   { id: 8, src: img8, full: img8Full },
+   { id: 9, src: img9, full: img9Full },
+   { id: 10, src: img10, full: img10Full },
+   { id: 11, src: img11, full: img11Full },
+   { id: 12, src: img12, full: img12Full },
+   { id: 13, src: img13, full: img13Full },
+   { id: 14, src: img14, full: img14Full },
+   { id: 15, src: img15, full: img15Full },
+   { id: 16, src: img16, full: img16Full },
+   { id: 17, src: img17, full: img17Full },
+   { id: 18, src: img18, full: img18Full },
+   { id: 19, src: img19, full: img19Full },
+   { id: 20, src: img20, full: img20Full },
+   { id: 21, src: img21, full: img21Full },
+   { id: 22, src: img22, full: img22Full },
+   { id: 23, src: img23, full: img23Full },
+   { id: 24, src: img24, full: img24Full },
+   { id: 25, src: img25, full: img25Full },
+   { id: 26, src: img26, full: img26Full },
+   { id: 27, src: img27, full: img27Full },
+   { id: 28, src: img28, full: img28Full },
+   { id: 29, src: img29, full: img29Full },
+   { id: 30, src: img30, full: img30Full },
+   { id: 31, src: img31, full: img31Full },
+   { id: 32, src: img32, full: img32Full },
+   { id: 33, src: img33, full: img33Full },
+   { id: 34, src: img34, full: img34Full },
+   { id: 35, src: img35, full: img35Full },
+   { id: 36, src: img36, full: img36Full },
+   { id: 37, src: img37, full: img37Full },
+   { id: 38, src: img38, full: img38Full },
+   { id: 39, src: img39, full: img39Full },
+   { id: 40, src: img40, full: img40Full },
+   { id: 41, src: img41, full: img41Full },
+   { id: 42, src: img42, full: img42Full },
+   { id: 43, src: img43, full: img43Full },
+   { id: 44, src: img44, full: img44Full },
+   { id: 45, src: img45, full: img45Full },
+   { id: 46, src: img46, full: img46Full },
+   { id: 47, src: img47, full: img47Full },
+   { id: 48, src: img48, full: img48Full },
+   { id: 49, src: img49, full: img49Full },
+   { id: 50, src: img50, full: img50Full },
+   { id: 51, src: img51, full: img51Full },
+   { id: 52, src: img52, full: img52Full },
+   { id: 53, src: img53, full: img53Full },
+   { id: 54, src: img54, full: img54Full },
+   { id: 55, src: img55, full: img55Full },
+   { id: 56, src: img56, full: img56Full },
+   { id: 57, src: img57, full: img57Full },
+   { id: 58, src: img58, full: img58Full },
+   { id: 59, src: img59, full: img59Full },
+   { id: 60, src: img60, full: img60Full },
+   { id: 61, src: img61, full: img61Full },
+   { id: 62, src: img62, full: img62Full },
+   { id: 63, src: img63, full: img63Full }
+]
+
+export default images
