c686d5946afc434cc3da3400b54e990f428079c3
Kevin Penney <kpenney@gmail.com>
2021-03-09 20:38:09 -0800

    seo optimized

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,20 +1,24 @@
 module.exports = {
    siteMetadata: {
-      title: 'Photography by Ricky Kelley'
+      title: 'Photography by Ricky Kelley',
+      description:
+         'I specialize in photojournalism which can be described as "capturing the uninterrupted moment.',
+      url: 'https://dev.penneyprojects.com',
+      image: '/RickyKelleyTiny.jpg'
    },
    plugins: [
       'gatsby-plugin-sass',
       'gatsby-plugin-react-helmet',
-      {
-         resolve: 'gatsby-source-filesystem',
-         options: {
-            name: 'data',
-            path: `${__dirname}/src/IMG/portfolio-tiny`
-         }
-      },
+      // {
+      //    resolve: 'gatsby-source-filesystem',
+      //    options: {
+      //       name: 'data',
+      //       path: `${__dirname}/src/IMG/portfolio-tiny`
+      //    }
+      // },
       'gatsby-plugin-styled-components',
       `gatsby-plugin-image`,
-      `gatsby-plugin-sharp`,
-      `gatsby-transformer-sharp`
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`
    ]
 }
--- client/src/Components/Head.jsx
+++ client/src/Components/Head.jsx
@@ -8,11 +8,33 @@ const Head = ({ title }) => {
          site {
             siteMetadata {
                title
+               description
+               url
+               image
             }
          }
       }
    `)
-   return <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+
+   const seo = data.site.siteMetadata
+   return (
+      <Helmet title={`${title} | ${seo.title}`}>
+         <meta name='robots' content='noindex, nofollow' />
+         <link rel='icon' href='/favicon.ico' />
+         <meta name='twitter:card' content='summary_large_image' />
+         <meta name='description' content={seo.description} />
+         <meta property='og:url' content={seo.url} />
+         <meta property='og:image' content={`${seo.url}${seo.image}`} />
+         <meta property='og:title' content={seo.title} />
+         <meta
+            property='og:description'
+            content='Photographic storytelling to convey whole experiences through images'
+         />
+         <meta property='og:type' content='website' />
+         <meta property='og:image:width' content='1200' />
+         <meta property='og:image:height' content='630' />
+      </Helmet>
+   )
 }
 
 export default Head
